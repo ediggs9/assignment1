a6b458f369db5d6f0f65d9becc92a8553961414e
ediggs9 <60329939+ediggs9@users.noreply.github.com>
2020-01-26 18:06:14 -0500

    Add files via upload

---
+++ assignment1.java
@@ -0,0 +1,14 @@
+//////////////
+//// CSE 02 Assignment 1
+//// Created January 26, 2020
+//// Author: Eddie Diggs
+///
+public class Assignment1{
+  
+  public static void main(String args []){
+    //Prints tree graphics to terminal window
+     System.out.println("  /#\\\t /#\\\t /#\\\n /#|#\\  /#|#\\   /#|#\\\n/##|##\\/##|##\\ /##|##\\\n   |\t  |\t  |");
+  }
+  
+  
+}
